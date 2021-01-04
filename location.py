c9cdc900a907fec9125ef1a21c598260fd63b6a1
readera193 <readera193@gmail.com>
2021-01-04 16:37:27 +0800

    refactor: 更改參數傳遞方式

--- location.py
+++ location.py
@@ -5,17 +5,18 @@ import sqlite3
 def location():
     conn = sqlite3.connect("motoPrice.db")
     cursor = conn.cursor()
-
-    # 燃油車維修地點
-    fuelCar = getData(cursor, "fuelCars")
-    # GOGORO 維修地點
-    gogoroRepair = getData(cursor, "gogoroRepairs")
-    # GOGORO 換電地點
-    gogoroReplacement = getData(cursor, "gogoroReplacements")
-    return render_template("location.html", fuelCar=fuelCar,
-                           gogoroRepair=gogoroRepair, gogoroReplacement=gogoroReplacement)
+    location = {
+        # 燃油車維修地點
+        "fuelCar": getData(cursor, "fuelCars"),
+        # GOGORO 維修地點
+        "gogoroRepair": getData(cursor, "gogoroRepairs"),
+        # GOGORO 換電地點
+        "gogoroReplacement": getData(cursor, "gogoroReplacements")
+    }
+    conn.close()
+    return render_template("location.html", **location)
 
 
 def getData(cursor, table_name):
     cursor.execute("SELECT * FROM {}".format(table_name))
-    return list( map(lambda tup: tup[0], cursor.fetchall()) )
+    return list(map(lambda tup: tup[0], cursor.fetchall()))
