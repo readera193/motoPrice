144dd1f496f4098e72d5621f17bf9572eef58127
TedYeh <58154758+TedYeh@users.noreply.github.com>
2021-01-10 00:41:50 +0800

    Update home.py

--- home.py
+++ home.py
@@ -1,9 +1,18 @@
 from flask import Flask, render_template, request
+from location import location
+from price import getPrice, test
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('home.html')
+    return render_template("home.html")
 
+@app.route("/location")
+def location_function():
+    return location()
 
+@app.route("/fixPrice", methods=["POST", "GET"])
+def fixPrice_function():
+    if not request.form.get("itemName"):return test('')
+    else:return test(request.form["itemName"])
